6e5650c616c89193cc9361025be12a719d1a2fd9
texotek <fdahmen@proton.me>
2024-09-18 16:23:26 +0200

    added model functionality

--- src/main/java/model/WordImagePair.java
+++ src/main/java/model/WordImagePair.java
@@ -1,2 +1,14 @@
-package model;public class WordImagePair {
+package model;
+
+import java.net.URL;
+
+public class WordImagePair {
+    private String word;
+    private URL imageURL;
+    public String getWord() {
+        return word;
+    }
+    public URL getImageURL() {
+        return imageURL;
+    }
 }
--- src/main/java/model/WordTrainer.java
+++ src/main/java/model/WordTrainer.java
@@ -1,2 +1,53 @@
-package model;public class WordTrainer {
+package model;
+
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
+public class WordTrainer {
+    final private List<WordImagePair> wordImagePairs;
+    private int selectedIndex = - 1;
+    private int countTotalGuess;
+    private int countCorrectGuess;
+    private int countWrongGuess;
+
+    public WordTrainer(List<WordImagePair> pairs) {
+        this.wordImagePairs = pairs;
+    }
+    public void selectNewPair() {
+        if(wordImagePairs.isEmpty())
+            return;
+
+        selectedIndex = (int) (Math.random() * wordImagePairs.size());
+    }
+    public void selectNewPair(int index) {
+        if(wordImagePairs.isEmpty() || index < 0 || index >= wordImagePairs.size())
+            return;
+
+        selectedIndex = index;
+    }
+
+    public URL getSelectedImageURL() throws IllegalStateException {
+        if(selectedIndex == -1)
+            throw new IllegalStateException("No word selected");
+
+        return wordImagePairs.get(selectedIndex).getImageURL();
+    }
+    public boolean guessSelectedWord(String word) throws IllegalStateException {
+        if (selectedIndex == -1)
+            throw new IllegalStateException("No word selected");
+
+        countTotalGuess++;
+        boolean isCorrect = wordImagePairs.get(selectedIndex).getWord().equals(word);
+
+        if(isCorrect) {
+            countCorrectGuess++;
+            selectedIndex = -1;
+        }
+        else {
+            countWrongGuess++;
+        }
+
+        return isCorrect;
+    }
 }
